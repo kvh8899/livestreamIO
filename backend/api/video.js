7687f18826784e2a555ed12cd44b3d446dbb2cb3
Kyle <kylevhuang@gmail.com>
2022-02-25 17:45:51 -0800

    gitignore

--- backend/api/video.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-router.use("/videos", (req, res) => {
-    res.sendFile('assets/sample.mp4', { root: __dirname });
-});
-
-module.exports = router;
